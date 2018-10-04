df2b078ac8542d256d0fd14dff4a008fe4ef64d5
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-10-04 11:52:44 -0400

    still finding remnants for searchkick

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,14 +1,13 @@
 class Ingredient < ApplicationRecord
-	searchkick word_start: [:name]
-
-	def search_data
-		{
-			name: name
-		}
-	end
-
-
 	belongs_to :recipes, optional: true
 	has_many :recipe_ingredients
 	has_many :recipes, through: :recipe_ingredients
+
+	def self.search(term)
+		if term
+			where('name LIKE ?', "%#{term}%").order('id DESC')
+		else
+			order('id DESC')
+		end
+	end
 end
